fa8fef3c7b2ccd8915cd07ae03f1e5ae6b30c6a8
Nathan Sroussi <Nathansroussi@gmail.com>
2022-04-29 17:17:41 +0200

    bdd:version-log

---
+++ BDD/files.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 29 avr. 2022 à 17:16
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `files`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `files_uploads`
+--
+
+CREATE TABLE `files_uploads` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(500) NOT NULL,
+  `unique_name` varchar(500) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `id_users` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `log_users`
+--
+
+CREATE TABLE `log_users` (
+  `id` int(11) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `Message` longtext NOT NULL,
+  `id_users` int(11) NOT NULL,
+  `ip` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pages_id`
+--
+
+CREATE TABLE `pages_id` (
+  `id` int(11) NOT NULL,
+  `nom_page` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `statistique`
+--
+
+CREATE TABLE `statistique` (
+  `id_stat` int(11) NOT NULL,
+  `ip` varchar(100) NOT NULL,
+  `compte` int(11) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `id_users` int(11) NOT NULL,
+  `pages_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nom_user` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
+  `cat` varchar(100) NOT NULL,
+  `date_user` varchar(100) NOT NULL,
+  `maxfile` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `nom_user`, `prenom`, `mail`, `mdp`, `cat`, `date_user`, `maxfile`) VALUES
+(1, 'root', 'root', 'root@gmail.com', 'root', 'admin', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_temp`
+--
+
+CREATE TABLE `users_temp` (
+  `id_users_temp` int(11) NOT NULL,
+  `nom` varchar(300) NOT NULL,
+  `prenom` varchar(300) NOT NULL,
+  `mail` varchar(300) NOT NULL,
+  `mdp` varchar(300) NOT NULL,
+  `statut` varchar(300) NOT NULL,
+  `date` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `files_uploads`
+--
+ALTER TABLE `files_uploads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `log_users`
+--
+ALTER TABLE `log_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `pages_id`
+--
+ALTER TABLE `pages_id`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `statistique`
+--
+ALTER TABLE `statistique`
+  ADD PRIMARY KEY (`id_stat`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users_temp`
+--
+ALTER TABLE `users_temp`
+  ADD PRIMARY KEY (`id_users_temp`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `files_uploads`
+--
+ALTER TABLE `files_uploads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `log_users`
+--
+ALTER TABLE `log_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `pages_id`
+--
+ALTER TABLE `pages_id`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `statistique`
+--
+ALTER TABLE `statistique`
+  MODIFY `id_stat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `users_temp`
+--
+ALTER TABLE `users_temp`
+  MODIFY `id_users_temp` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
